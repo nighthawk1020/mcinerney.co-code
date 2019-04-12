3e3237bfaafb41d4629af7d93c3e8878adf8d6c7
Michael McInerney <mmac1020@gmail.com>
2019-04-11 23:07:58 -0400

    testing server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../index.html'));
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 const port = process.env.PORT | 3000;
 app.listen(port, (err) => {
