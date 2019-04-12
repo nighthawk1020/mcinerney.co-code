623cecc58f540826f8ac9f5a0e8c04c46e05202e
Michael McInerney <mmac1020@gmail.com>
2019-04-11 22:12:12 -0400

    set up node server for testing

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const path = require('path');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../index.html'));
+});
+const port = process.env.PORT | 3000;
+app.listen(port, (err) => {
+    err ? console.log(err) : console.log('server started on port: ' + port);
+});
