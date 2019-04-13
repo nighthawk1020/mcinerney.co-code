ef7d95dc759261cf573a1356110e8c3458457f16
Michael McInerney <mmac1020@gmail.com>
2019-04-13 11:49:41 -0400

    added CORS

--- server.js
+++ server.js
@@ -2,9 +2,12 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const path = require('path');
+const cors = require('cors');
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
