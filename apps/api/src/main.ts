918a25c9cf52326d076f6185d086df9b05605ec4
nighthawk1020 <nighthawk1020@gmail.com>
2019-06-01 08:57:54 -0400

    removed google cloud storage stuff

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -3,22 +3,6 @@ import { AppModule } from './app/app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 
-import { Storage } from '@google-cloud/storage';
-
-const storage = new Storage(
-  {
-    projectId: 'sturdy-landing-165622',
-    keyFilename: join(__dirname, '..', 'project-key.json')
-  }
-);
-
-storage
-  .getBuckets()
-  .then(results => {
-    const buckets = results[0];
-    
-  })
-
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useStaticAssets(join(__dirname, '../','website-code-angular'));
