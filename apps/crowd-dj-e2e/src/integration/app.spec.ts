ba8374b88e1b5f9cdb717912413d11a26312bb16
nighthawk1020 <nighthawk1020@gmail.com>
2019-07-21 14:15:55 -0400

    removed cypress for security concerns

--- apps/crowd-dj-e2e/src/integration/app.spec.ts
+++
@@ -1,9 +0,0 @@
-import { getGreeting } from '../support/app.po';
-
-describe('crowd-dj', () => {
-  beforeEach(() => cy.visit('/'));
-
-  it('should display welcome message', () => {
-    getGreeting().contains('Welcome to crowd-dj!');
-  });
-});
