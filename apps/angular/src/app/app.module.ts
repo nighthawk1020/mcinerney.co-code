b84502e375ce6a2be7b5d304072d03cfa505a071
nighthawk1020 <nighthawk1020@gmail.com>
2020-01-18 08:31:24 -0500

    first commit to update to rc9

--- apps/angular/src/app/app.module.ts
+++ apps/angular/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing/app-routing.module';
+import { AngularLandingModule } from './core/landing/angular-landing.module';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
+    AngularLandingModule,
     AppRoutingModule,
     BrowserModule,
     HttpClientModule
---
+++ apps/angular/src/app/core/landing/angular-landing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+
+import { BrowserModule } from '@angular/platform-browser';
+import { AngularLandingComponent } from './angular-landing.component';
+
+@NgModule({
+  declarations: [AngularLandingComponent],
+  imports: [
+    BrowserModule
+  ],
+  providers: []
+})
+export class AngularLandingModule {}
