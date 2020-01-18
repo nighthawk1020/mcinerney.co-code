e489c5a5ed7a8cd0e4f8f3ebd2a8fd43a6c94f81
nighthawk1020 <nighthawk1020@gmail.com>
2020-01-18 08:24:58 -0500

    fixed new imports

--- apps/crowd-dj/src/app/common/angular-material/angular-material.module.ts
+++ apps/crowd-dj/src/app/common/angular-material/angular-material.module.ts
@@ -1,6 +1,8 @@
 
 import { NgModule } from '@angular/core';
-import { MatAutocompleteModule, MatButtonModule, MatInputModule } from '@angular/material';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
 import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule(
